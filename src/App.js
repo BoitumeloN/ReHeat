2c14624ba41d54912779046326ba299e4e7e93d4
boitumelo <boitumelok.ngwenya@gmail.com>
2024-08-12 15:41:31 +0200

    Adding tailwind

--- src/App.js
+++ src/App.js
@@ -10,21 +10,21 @@ const App = () => {
     <div>
       <div class="header-container">
         <div className = "landing_main_header">
-        <h1>ReHeats</h1>
+        <h1 className='text-red'>ReHeats</h1>
       </div>
         <div className = "landing_main_header_left">
-          <a href="#">My Spots</a>
+          <a href="#">My Spots</a>  
           <a href="#">Login</a>
         </div>
     </div>
     <div className = "body_text_center">
-      <h3>THE FOOD APP</h3>
+      <h3>APP</h3>
       <p>Revisit your favorite flavors and discover new culinary gems with our food spots review app.</p>
       <p> Whether you're craving a beloved dish from a familiar restaurant or eager to explore hidden gems around</p>
     </div>
     <SearchComponent onPlaceSelect={setLocation} />
     <div class = "about_for_foodies_center">
-      <h6>built for foodies</h6>
+      <h6 className="text-white bg-gray-800 p-4 font-sans">built for foodies</h6>
     </div>
     </div>
   );
