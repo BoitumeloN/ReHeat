5b435c8671d41dec8f205e87f3fb91ebf58cb01b
boitumelo <boitumelok.ngwenya@gmail.com>
2024-08-13 16:06:23 +0200

    Taiwind CSS working

--- src/App.js
+++ src/App.js
@@ -8,23 +8,27 @@ const App = () => {
 
   return (
     <div>
-      <div class="header-container">
-        <div className = "landing_main_header">
-        <h1 className='text-blue'>ReHeats</h1>
-      </div>
-        <div className = "landing_main_header_left">
-          <a href="#">My Spots</a>  
-          <a href="#">Login</a>
+      <div class="flex w-full">
+        <div className = "place-content-between">
+        <h1 className='text-white px-8'>ReHeats</h1>
+      </div>  
+        <div className = "text-white top-0 right-0">
+            <a className='px-8 text-right'
+              href="#">My Spots
+            </a>  
+            <a className='px-8 text-right'
+              href="#">Login
+            </a>
         </div>
     </div>
-    <div className = "body_text_center">
-      <h3>APP</h3>
-      <p>Revisit your favorite flavors and discover new culinary gems with our food spots review app.</p>
-      <p> Whether you're craving a beloved dish from a familiar restaurant or eager to explore hidden gems around</p>
+    <div className = "">
+      <h3 className='text-white text-center w-100 py-20'>APP</h3>
+      <p className='text-white text-center w-100'>Revisit your favorite flavors and discover new culinary gems with our food spots review app.</p>
+      <p className='text-white text-center w-100'> Whether you're craving a beloved dish from a familiar restaurant or eager to explore hidden gems around</p>
     </div>
     <SearchComponent onPlaceSelect={setLocation} />
     <div class = "about_for_foodies_center">
-      <h6 className="text-white bg-gray-800 p-4 font-sans">built for foodies</h6>
+      <h6 className="text-white text-center bg-black-800 p-4 font-sans ">built for foodies</h6>
     </div>
     </div>
   );
